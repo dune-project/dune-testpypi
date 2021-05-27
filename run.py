0d3ffdabf3153317187ab24f9b2e513725e5bc15
dedner <a.s.dedner@warwick.ac.uk>
2021-05-27 22:47:46 +0100

    improve ci

--- run.py
+++ run.py
@@ -4,9 +4,11 @@ import glob
 
 # Creating the tuple of all the processes
 all_processes = glob.glob("*.py")
+disable = ["3dexample.py", "interpolation.py"]
 
 # This block of code enables us to call the script from command line.
 def execute(process):
+    if process in disable: return [process,'disabled']
     ret = os.system(f'python {process}')
     return [process,ret]
 
