66748e7d4cd6b4f790631696017c75aea99ebf3f
dedner <a.s.dedner@warwick.ac.uk>
2021-05-27 20:49:04 +0100

    improve ci

--- run.py
+++ run.py
@@ -3,11 +3,14 @@ import os
 import glob
 
 # Creating the tuple of all the processes
-all_processes = glob.glob("*.py")
+all_processes = glob.glob("laplace-*.py")
 
 # This block of code enables us to call the script from command line.
 def execute(process):
-    os.system(f'python {process}')
+    ret = os.system(f'python {process}')
+    return [process,ret]
 
 process_pool = multiprocessing.Pool(processes = 8)
-process_pool.map(execute, all_processes)
+ret = process_pool.map(execute, all_processes)
+for r in ret:
+    print(r)
