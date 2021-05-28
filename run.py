ba4964364abcf487f3aa30467342d3e16819364f
dedner <a.s.dedner@warwick.ac.uk>
2021-05-28 18:09:30 +0100

    improve ci

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import os, glob, sys
 
 # Creating the tuple of all the processes
 all_processes = glob.glob("*.py")
-all_processes = ["boundary.py"]
 disable = ["3dexample.py", "interpolation.py"]
 
 # This block of code enables us to call the script from command line.
@@ -12,7 +11,7 @@ def execute(process):
     ret = os.system(f'python {process}')
     return [process,ret]
 
-process_pool = multiprocessing.Pool(processes = 4)
+process_pool = multiprocessing.Pool(processes = 8)
 ret = process_pool.map(execute, all_processes)
 ret.sort()
 for r in ret:
