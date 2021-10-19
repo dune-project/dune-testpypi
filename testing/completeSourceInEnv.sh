f6b8fbccc9456338d386a4134cf7316c14c10e3c
dedner <a.s.dedner@warwick.ac.uk>
2021-10-19 13:56:15 +0100

    minor

--- testing/completeSourceInEnv.sh
+++ testing/completeSourceInEnv.sh
@@ -1,6 +1,6 @@
 python -m venv dune-env 
 . dune-env/bin/activate
-repos/dune-common/bin/dunecontrol all
+$HOME/repos/dune-common/bin/dunecontrol all
 python -m dune.fem
 cd fem_tutorial
 python concepts.py
--- testing/completeSourceNoEnv.sh
+++ testing/completeSourceNoEnv.sh
@@ -1,5 +1,5 @@
-repos/dune-common/bin/dunecontrol all
-alias runDune=repos/dune-common/build-cmake/run-in-dune-env
+$HOME/repos/dune-common/bin/dunecontrol all
+alias runDune=$HOME/repos/dune-common/build-cmake/run-in-dune-env
 runDune python -m dune.fem
 cd fem_tutorial
 python concepts.py
