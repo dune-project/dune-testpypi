79e96bfd25a941f71e9aabbeff090a8ef4bf134c
dedner <a.s.dedner@warwick.ac.uk>
2021-10-21 00:08:53 +0100

    improve tests

--- testing/installPlusSource.sh
+++ testing/installPlusSource.sh
@@ -1,33 +1,31 @@
 python -m venv dune-env
 . dune-env/bin/activate
-pip install --pre --find-links file://$PWD/../dist dune.grid
+pip install --pre --find-links file://$PWD/../dist dune.alugrid dune.istl dune.localfunctions
 
 # add dune-fem and test
-cd ../repos
-dune-common/bin/dunecontrol --only=dune-fem all
-cd -
+cp -r ../repos/dune-fem .
+dunecontrol --only=dune-fem all
 python -m dune.fem
 cd fem_tutorial
 pip list
 python concepts.py
 cd ..
+rm -rf fem_tutorial
 
+## do this with some other packages e.g. dune-mmsh
 # install dune-alugrid and test again
-pip install --pre --find-links file://$PWD/../dist dune.alugrid
-cd ../repos
-dune-common/bin/dunecontrol --only=dune-fem all
-cd -
-python -m dune.fem
-cd fem_tutorial
-pip list
-python laplace-adaptive.py
-cd ..
+# pip install --pre --find-links file://$PWD/../dist dune.alugrid
+# dunecontrol --only=dune-fem all
+# python -m dune.fem
+# cd fem_tutorial
+# pip list
+# python laplace-adaptive.py
+# cd ..
 
 # add dune-alugrid from source and test again
-cd ../repos
-dune-common/bin/dunecontrol --only=dune-alugrid all
-dune-common/bin/dunecontrol --only=dune-fem all
-cd -
+cp -r ../repos/dune-alugrid .
+dunecontrol --only=dune-alugrid all
+dunecontrol --only=dune-fem all
 python -m dune.fem
 cd fem_tutorial
 pip list
--- testing/mmeshFromSource.sh
+++ testing/mmeshFromSource.sh
@@ -3,7 +3,7 @@ python3 -m venv dune-env
 pip install --pre --find-links file://$PWD/../dist dune.fem
 
 # add dune-mmesh and test
-cd ../repos
+cp -r ../repos/dune-mmesh .
 # dunecontrol --only=dune-mmesh --opts=dune-mmesh/cmake/config.opts all
 dunecontrol --only=dune-mmesh all
 cd dune-mmesh/scripts
