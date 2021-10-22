07769e5767f6064bc43f277a5fb26aa0566b8a71
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2021-10-22 12:31:27 +0200

    Add mmesh test again.

---
+++ testing/mmeshFromSource.sh
@@ -0,0 +1,22 @@
+python3 -m venv dune-env
+. dune-env/bin/activate
+pip install --pre --find-links file://$PWD/../dist dune.fem
+
+# install dune-mmesh
+. ../package
+
+echo "cloning dune-mmesh with branch $1"
+git clone --depth 1 -b $1 https://gitlab.dune-project.org/samuel.burbulla/dune-mmesh.git
+echo "done"
+
+cd dune-mmesh
+echo $PWD
+package $PWD/../..
+echo "PACKAGED"
+cd ..
+pip install --pre --find-links file://$PWD/dune-mmesh/dist dune.mmesh
+pip list
+
+# test that it can be used within dune-fem
+cd dune-mmesh/scripts
+python test.py
