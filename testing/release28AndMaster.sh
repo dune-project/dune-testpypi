f1af59f2283da8398de5b131e4f18d08729e710e
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2021-10-29 16:00:38 +0200

    Build from source.

--- testing/release28AndMaster.sh
+++ testing/release28AndMaster.sh
@@ -1,22 +1,20 @@
 python3 -m venv dune-env
 . dune-env/bin/activate
 
-pip install dune-fem==2.8.0
-
-# install dune-mmesh
-. ../package
+echo "cloning core 2.8"
+git clone --depth 1 -b releases/2.8 https://gitlab.dune-project.org/core/dune-common.git
+git clone --depth 1 -b releases/2.8 https://gitlab.dune-project.org/core/dune-geometry.git
+git clone --depth 1 -b releases/2.8 https://gitlab.dune-project.org/core/dune-localfunctions.git
+git clone --depth 1 -b releases/2.8 https://gitlab.dune-project.org/core/dune-istl.git
+git clone --depth 1 -b releases/2.8 https://gitlab.dune-project.org/core/dune-grid.git
+git clone --depth 1 -b releases/2.8 https://gitlab.dune-project.org/extensions/dune-alugrid.git
+git clone --depth 1 -b releases/2.8 https://gitlab.dune-project.org/dune-fem/dune-fem.git
 
 echo "cloning dune-mmesh with branch $1"
 git clone --depth 1 -b $1 https://gitlab.dune-project.org/samuel.burbulla/dune-mmesh.git
-echo "done"
 
-cd dune-mmesh
-echo $PWD
-package $PWD/../..
-echo "PACKAGED"
-cd ..
-pip install --pre --find-links file://$PWD/dune-mmesh/dist dune.mmesh
-pip list
+./dune-common/bin/dunecontrol --opts=dune-mmesh/cmake/config.opts all
+./dune-common/bin/dunecontrol --opts=dune-mmesh/cmake/config.opts make install_python
 
 # test that it can be used within dune-fem==2.8.0
 cd dune-mmesh/scripts
