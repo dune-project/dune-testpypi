60317796942ddcf2114554e5133818fed5072dba
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2021-10-29 13:55:30 +0200

    Add also istl and localfunctions.

--- testing/release28AndMaster.sh
+++ testing/release28AndMaster.sh
@@ -2,6 +2,8 @@ python3 -m venv dune-env
 . dune-env/bin/activate
 
 pip install dune-grid==2.8.0
+pip install dune-istl==2.8.0
+pip install dune-localfunctions==2.8.0
 pip install --pre --find-links file://$PWD/../dist dune.fem
 
 python -m dune.fem
