a61653a3aa1fd1553e0a00b5a6e3656d8bf6c3f9
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2021-10-29 15:11:23 +0200

    Use mmesh as non 2.8 module.

--- testing/release28AndMaster.sh
+++ testing/release28AndMaster.sh
@@ -1,12 +1,23 @@
 python3 -m venv dune-env
 . dune-env/bin/activate
 
-pip install dune-grid==2.8.0
-pip install dune-istl==2.8.0
-pip install dune-localfunctions==2.8.0
-pip install --pre --find-links file://$PWD/../dist dune.fem
+pip install dune-fem==2.8.0
 
-python -m dune.fem
-cd fem_tutorial
-python concepts.py
+# install dune-mmesh
+. ../package
+
+echo "cloning dune-mmesh with branch $1"
+git clone --depth 1 -b $1 https://gitlab.dune-project.org/samuel.burbulla/dune-mmesh.git
+echo "done"
+
+cd dune-mmesh
+echo $PWD
+package $PWD/../..
+echo "PACKAGED"
 cd ..
+pip install --pre --find-links file://$PWD/dune-mmesh/dist dune.mmesh
+pip list
+
+# test that it can be used within dune-fem==2.8.0
+cd dune-mmesh/scripts
+python test.py
