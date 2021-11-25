fea82c1815b49ce3e533af794f4516974091e6bc
dedner <a.s.dedner@warwick.ac.uk>
2021-11-25 08:50:29 +0000

    [fix]

--- testpypi.py
+++ testpypi.py
@@ -1,9 +1,6 @@
 import requests, sys
 pypi_url = "https://pypi.org"
 name = sys.argv[1] # "dune-grid"
-try:
-    version = sys.argv[2] # "v2.8.1"
-    request = requests.get(f"{pypi_url}/pypi/{name}/json")
-    sys.exit(0 if version in request.json()["releases"].keys() else 1)
-except IndexError:
-    sys.exit(1)
+version = sys.argv[2] # "v2.8.1"
+request = requests.get(f"{pypi_url}/pypi/{name}/json")
+sys.exit(0 if version in request.json()["releases"].keys() else 1)
