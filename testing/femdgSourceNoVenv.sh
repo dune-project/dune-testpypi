7f842ca3950ee1bb6c3b115328961a29578bcd23
dr-robertk <dr-robertk@posteo.org>
2022-03-07 19:21:23 +0100

    Add custom config.opts for dune-fem-dg test.

--- testing/femdgSourceNoVenv.sh
+++ testing/femdgSourceNoVenv.sh
@@ -1,6 +1,23 @@
 cd ../repos
-dune-common/bin/dunecontrol --module=dune-fem-dg all
 
+FLAGS="-O3 -DNDEBUG"
+
+echo "\
+DUNEPATH=`pwd`
+BUILDDIR=build-cmake
+CMAKE_FLAGS=\"-DCMAKE_CXX_FLAGS=\\\"$FLAGS\\\"  \\
+ -DDUNE_ENABLE_PYTHONBINDINGS=ON \\
+ -DALLOW_CXXFLAGS_OVERWRITE=ON \\
+ -DDUNE_PYTHON_USE_VENV=OFF \\
+ -DADDITIONAL_PIP_PARAMS="-upgrade" \\
+ -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE \\
+ -DDISABLE_DOCUMENTATION=TRUE \\
+ -DCMAKE_DISABLE_FIND_PACKAGE_Vc=TRUE \\
+ -DCMAKE_DISABLE_FIND_PACKAGE_LATEX=TRUE\" " > femdg-config.opts
+
+#-DCMAKE_LD_FLAGS=\\\"$PY_LDFLAGS\\\" \\
+
+dune-common/bin/dunecontrol --opts=femdg-config.opts --module=dune-fem-dg all
 
 cd dune-fem-dg/pydemo/euler
 python testdg.py
