705b041ed39f0e3d8772a4d7c03914ccf74b9f19
dedner <a.s.dedner@warwick.ac.uk>
2022-03-09 16:19:52 +0000

    [fix]

--- testing/installPlusSource.sh
+++ testing/installPlusSource.sh
@@ -15,15 +15,8 @@ dunecontrol --only=dune-fem all
 python -m dune.fem
 cd fem_tutorial
 pip list
-python concepts.py
+/usr/bin/time python concepts.py
 cd ..
-
-# # install dune-fem-dg and test again
-# cp -r ../repos/dune-fem-dg .
-# dunecontrol --only=dune-fem-dg all
-# cd fem_tutorial
-# pip list
-# cd ..
 
 # install polygongrid and test that can be used within dune-fem
 . ../package
@@ -41,14 +34,14 @@ cd ..
 pip install --pre --find-links file://$PWD/../dist dune.polygongrid
 pip list
 testScript="\
-from dune.polygongrid import polygonGrid ;\
+from dune.polygongrid import polygonGrid as leafGridView ;\
 from dune.grid import cartesianDomain ;\
 print(\"STARTED\") ;\
-view = polygonGrid( cartesianDomain([0,0],[1,1],[10,10]), dualGrid=False ) ;\
+view = leafGridView( cartesianDomain([0,0],[1,1],[10,10]), dualGrid=False ) ;\
 print(\"have grid\",view.size(0)) ;\
-from dune.fem.space import finiteVolume ;\
+from dune.fem.space import finiteVolume as space ;\
 print(\"SPACE\") ;\
-spc = finiteVolume(view) ;\
+spc = space(view) ;\
 print(\"have space\",spc.size) \
 "
 python -c "$testScript"
@@ -59,13 +52,26 @@ dunecontrol --only=dune-alugrid all
 # at the moment we need to remove the build dirs of the source modules
 # depending on dune-alugrid because their metadata will point to the
 # installed dune-alugrid and this leads to a mismatch failure
-rm -rf dune-fem/build-cmake dune-fem-dg
+rm -rf dune-fem/build-cmake
 # only need to rebuild dune-fem
 dunecontrol --only=dune-fem all
+pip list
+testScript="\
+from dune.alugrid import aluSimplexGrid as leafGridView ;\
+from dune.grid import cartesianDomain ;\
+print(\"STARTED\") ;\
+view = leafGridView( cartesianDomain([0,0,0],[1,1,1],[10,10,10]) ) ;\
+print(\"have grid\",view.size(0)) ;\
+from dune.fem.space import lagrange as space ;\
+print(\"SPACE\") ;\
+spc = space(view) ;\
+print(\"have space\",spc.size) \
+"
+python -c "$testScript"
+
+
+# we can rerun concepts.py which should be fast
 cd fem_tutorial
 pip list
-# at the moment dune packages don't provide a __version__ attribute.
-# python -c "import dune.algrid ; assert not 'dev' in dune.alugrid.__version__"
-#python laplace-adaptive.py
-python mcf-algorithm.py
+/usr/bin/time python concepts.py
 cd ..
