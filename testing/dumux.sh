140480a4b17cca820851a914da4637e9e90563cc
dedner <a.s.dedner@warwick.ac.uk>
2022-07-21 10:38:21 +0100

    minor change to dumux script

--- testing/dumux.sh
+++ testing/dumux.sh
@@ -1,19 +1,20 @@
 # this tests dumux as external package which provides
 # a Python package outside the dune namespace
 
-# dumux Python bindings currently only work with shared libs
-export DUNE_CMAKE_FLAGS=-DBUILD_SHARED_LIBS=ON
 dumux_url="https://git.iws.uni-stuttgart.de/dumux-repositories/dumux.git"
 
 # setup external venv and install the prepared packages needed
 # for dumux
 python3 -m venv dune-env
 source dune-env/bin/activate
-pip install mpi4py
-pip install --pre --find-links file://$PWD/../dist dune.common dune.grid dune.geometry dune.localfunctions dune.istl
+pip install mpi4py requests
+pip install --pre --find-links file://$PWD/../dist dune.common dune.grid dune.geometry dune.localfunctions dune.istl dune.alugrid
 
 # source the clonemodule function
 source ../package
+
+# dumux Python bindings currently only work with shared libs
+export DUNE_CMAKE_FLAGS=-DBUILD_SHARED_LIBS=ON
 
 # clone dumux
 echo "cloning dumux master"
