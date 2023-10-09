791062bfa27f2895c1e74b626017bd87602aa745
dr-robertk <dr-robertk@posteo.org>
2023-10-09 13:00:56 +0200

    Try to pipelines.

--- testing/completeSourceNoEnv.sh
+++ testing/completeSourceNoEnv.sh
@@ -3,6 +3,13 @@ dune-common/bin/dunecontrol --module=dune.grid all
 runDune="$PWD/dune-common/build-cmake/run-in-dune-env"
 cd -
 
+if [ "$3" == "macOS" ]; then
+  # dune-common dependencies
+  $runDune pip3 install -U jinja2 wheel setuptools mpi4py numpy ninja
+  # dune-fem dependencies
+  $runDune pip3 install -U scipy fenics-ufl matplotlib
+fi
+
 $runDune pip3 install matplotlib
 
 $runDune python3 -m dune.grid
