15b75ecdbcb0218524c99e6d00f1ea0de9f2171a
dedner <a.s.dedner@warwick.ac.uk>
2024-03-07 13:29:59 +0000

    check that a tag is a legal version string

--- testpyversion.py
+++ testpyversion.py
@@ -2,4 +2,7 @@
 import re,sys
 def is_canonical(version):
     return re.match(r'^([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?$', version) is not None
-exit( not is_canonical( sys.argv[1] ) )
+try:
+    exit( not is_canonical( sys.argv[1] ) )
+except:
+    exit(1)
