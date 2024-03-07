62156fc6a36e910f2dfbbdbd46ec1635492a2567
dedner <a.s.dedner@warwick.ac.uk>
2024-03-07 10:30:28 +0000

    test that tag is a legal python version otherwise go back to devDate format

---
+++ testpyversion.py
@@ -0,0 +1,5 @@
+# https://packaging.python.org/en/latest/specifications/version-specifiers/#version-specifiers-regex
+import re,sys
+def is_canonical(version):
+    return re.match(r'^([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?$', version) is not None
+exit( not is_canonical( sys.argv[1] ) )
