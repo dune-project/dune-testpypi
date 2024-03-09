fa99105cc707afeffcd1d9aafc960144ca4f0f84
dedner <a.s.dedner@warwick.ac.uk>
2024-03-09 10:13:00 +0000

    minor

--- testpyversion.py
+++ testpyversion.py
@@ -3,6 +3,7 @@ import re,sys
 def is_canonical(version):
     return re.match(r'^([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?$', version) is not None
 try:
-    exit( not is_canonical( sys.argv[1] ) )
+    ret = not is_canonical( sys.argv[1] )
 except:
-    exit(1)
+    ret = 2
+exit(ret)
