45949c5cc656cecf5fe69fd5e398b39dbe98323f
A Dedner <a.s.dedner@warwick.ac.uk>
2024-11-14 17:06:14 +0000

    some output

--- testing/installPlusSource.sh
+++ testing/installPlusSource.sh
@@ -15,7 +15,9 @@ import sys ;\
 import petsc4py ;\
 petsc4py.init(sys.argv) ;\
 from petsc4py import PETSc ;\
-print(petsc4py.__version__) ;\
+print(\"===============================\") ;\
+print(\"petsc4py version:\",petsc4py.__version__) ;\
+print(\"===============================\") ;\
 "
 python -c "$testScript"
 
