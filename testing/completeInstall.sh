8766dbf416b289c601869605f6ba5cfd841fcf1e
A Dedner <a.s.dedner@warwick.ac.uk>
2026-08-04 16:45:32 +0100

    [bugfix] attempt

--- testing/completeInstall.sh
+++ testing/completeInstall.sh
@@ -1,14 +1,14 @@
 python3 -m venv dune-env
 . dune-env/bin/activate
-pip install matplotlib mpi4py scikit-build
+pip install matplotlib mpi4py
 
-pip install --pre --find-links file://$PWD/../dist dune.grid
+pip install --find-links file://$PWD/../dist dune.grid
 python -m dune.grid
 cd grid_tutorial
 python example.py
 cd ..
 
-pip install --pre --find-links file://$PWD/../dist dune.vem
+pip install --find-links file://$PWD/../dist dune.vem
 python -m dune.fem
 cd fem_tutorial
 python laplace-adaptive.py
